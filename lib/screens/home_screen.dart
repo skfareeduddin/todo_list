1a26f52b829220d595306e197f084fe866e8041f
Syed Khaja Fareeduddin <skfareeduddin5@gmail.com>
2024-01-05 23:09:21 +0530

    Updated HomeScreen UI.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list/providers/note_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TODO List'),
+        centerTitle: true,
+      ),
       body: SafeArea(
-        child: Placeholder(),
+        child: Consumer<NoteProvider>(
+          builder: (context, noteProvider, child) {
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                final notes = noteProvider.notes[index];
+                return ListTile(
+                  title: Text(notes.title),
+                  subtitle: Text(notes.content),
+                  trailing: Text('Last updated at: ${notes.timeStamp}'),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
